bbe00addeb5723c95cac51518c1c80e32cc0c216
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-25 11:21:11 -0300

    Template do PEdreiro para Todos, ajeitando problemas

--- src/Http/Controllers/Admin/ActionController.php
+++ src/Http/Controllers/Admin/ActionController.php
@@ -13,7 +13,7 @@ class ActionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $modelForActions = Action::getModels();
 
