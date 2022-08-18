d94345bfba8cdf2d7488374dd1e0d10321f3bfc4
Ricardo R. Sierra <sierra.csi@gmail.com>
2022-08-18 13:21:26 -0300

    Metrics

--- src/Actions/Instagram/GetInbox.php
+++ src/Actions/Instagram/GetInbox.php
@@ -10,17 +10,35 @@ class GetInbox extends Instagram
 
     public function executeForEach($target)
     {
-        sleep(2); // Delay to mimic user
-        $followers = [];
-        $account = $this->executor->getAccount($target);
-        sleep(1);
+        // sleep(2); // Delay to mimic user
+        // // $followers = [];
+        // // $account = $this->executor->getAccount($target);
+        // sleep(1);
+// $this->executor->pending();
+$inbox = $this->executor->getInbox();
 
+
+// $itens = $inbox['itens'];
+$new_stories = $inbox['new_stories'];
+$old_stories = $inbox['old_stories'];
+$this->getStories($new_stories);
+$this->getStories($old_stories);
+
+$counts = $inbox['counts'];
+$last_checked = $inbox['last_checked'];
+$priority_stories = $inbox['priority_stories'];
+
+// $this->executor->seenInbox();
         dd(
-$instagram->pending(),
-$inbox = $instagram->getInbox(),
-$instagram->seenInbox(),
 
-    $inbox
+            // $itens,
+            count($new_stories),
+            count($old_stories),
+
+
+            $counts,
+            $last_checked ,
+            $priority_stories
 );
         // if (!$accountModel = Account::where(
         //     [
@@ -38,4 +56,124 @@ $instagram->seenInbox(),
         // }
         // $this->account->relations()->attach($accountModel, ['relation_type' => 'followers']);
     }
+
+    /**
+     * "counts" => array:13 [
+    "new_posts" => 0
+    "shopping_notification" => 0
+    "relationships" => 0
+    "close_friend_activities" => 0
+    "fundraiser" => 0
+    "campaign_notification" => 0
+    "activity_feed_dot_badge" => 0
+    "usertags" => 0
+    "comment_likes" => 0
+    "likes" => 2
+    "comments" => 0
+    "photos_of_you" => 0
+    "requests" => 0
+  ]
+last_checked" => 1660806405.9865
+  "priority_stories"
+     */
+    public function countsGet($count) {
+        dd(
+            $count
+        );
+    }
+    // new_stories =  "type" => 1
+    // "story_type" => 60
+    // "args" => array:15 [
+    //   "text" => "ricardorsierra liked your photo."
+    //   "links" => array:1 [ …1]
+    //   "users" => array:1 [ …1]
+    //   "media_destination" => "media?id=2894845802032896851_18021751522"
+    //   "destination" => "media?id=2894845802032896851_18021751522"
+    //   "profile_id" => 500550176
+    //   "profile_image" => "https://instagram.fstu1-1.fna.fbcdn.net/v/t51.2885-19/279012508_119454270712806_8621865750
+    // 704364831_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fstu1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=7-7t9n0WRfQAX9YpGDd&ed
+    // m=AL4D0a4BAAAA&ccb=7-5&oh=00_AT_vsBUdlpU9vjdTxaurJuREXP5-tRhQNBuuqdjuFpeSmQ&oe=63043C52&_nc_sid=712cc3"
+    //   "media" => array:1 [ …1]
+    //   "images" => array:1 [ …1]
+    //   "timestamp" => 1660824648.1463
+    //   "tuuid" => "cb5fe688-1eee-11ed-8080-808080808080"
+    //   "af_candidate_id" => 10670689
+    //   "profile_name" => "ricardorsierra"
+    //   "latest_reel_media" => 0
+    //   "latest_reel_seen_time" => 0
+    // ]
+    public function getStories(array $storys) {
+        foreach ($storys as $story) {
+                $this->getStory($story);
+        }
+    }
+    public function getStory(array $story) {
+
+        $type = $story['type'];
+        $story_type = $story['story_type'];
+        $args =  $story['args'];
+        // $args['text'];
+        if (isset($args['users']) && !empty($args['users'])) {
+            $this->getUsers($args['users']);
+        }
+    }
+    public function getUsers(array $users) {
+        foreach ($users as $user) {
+                $this->registerUserByArray($user);
+        }
+    }
 }
+// "counts" => []
+// old_stories" => array:98 [
+//     0 => array:5 [
+//       "type" => 1
+//       "story_type" => 60
+//       "args" => array:15 [
+//         "text" => "_joao_noronha_ liked your post."
+//         "links" => array:1 [ …1]
+//         "users" => array:1 [ …1]
+//         "media_destination" => "media?id=2906641389124641855_18021751522"
+//         "destination" => "media?id=2906641389124641855_18021751522"
+//         "profile_id" => 7803496907
+//         "profile_image" => "https://instagram.fstu1-1.fna.fbcdn.net/v/t51.2885-19/295846561_169551448903936_9356614669
+// 2566051_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fstu1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=URj532dRWbwAX_dizYc&edm=
+// AL4D0a4BAAAA&ccb=7-5&oh=00_AT-8mkRBLCIke3lJsklbbZIn35mccXo62JufNzFcNZukQQ&oe=63054A20&_nc_sid=712cc3"
+//         "media" => array:1 [ …1]
+//         "images" => array:1 [ …1]
+//         "timestamp" => 1660799086.0828
+//         "tuuid" => "47328edc-1eb3-11ed-8080-808080808080"
+//         "af_candidate_id" => 6273127
+//         "profile_name" => "_joao_noronha_"
+//         "latest_reel_media" => 0
+//         "latest_reel_seen_time" => 0
+//       ]
+//       "counts" => []
+//       "pk" => "ub396XLpWUBy75g9sUd58OgJyFk="
+//     ]
+
+
+
+
+
+
+
+
+// public function data()
+// {
+// }
+// }
+// items[
+// "user" => array:10 [ …10]
+// "algorithm" => "unknown"
+// "social_context" => "Followed by rafael_couto.g + 1 more"
+// "icon" => ""
+// "caption" => ""
+// "media_ids" => []
+// "thumbnail_urls" => []
+// "large_urls" => []
+// "media_infos" => []
+// "value" => 0.00101079
+// "followed_by" => false
+// "is_new_suggestion" => false
+// "uuid" => "18021751522|1660816057"
+// ]
--- src/Actions/Instagram/Instagram.php
+++ src/Actions/Instagram/Instagram.php
@@ -5,15 +5,56 @@ namespace Operador\Actions\Instagram;
 use Illuminate\Support\Facades\Facade;
 use Phpfastcache\Helper\Psr16Adapter;
 use Integrations\Connectors\Connector;
+use  Telefonica\Models\Digital\Account;
+use Telefonica\Services\SocialService;
+use  Telefonica\Models\Digital\Password;
+
+// @todo
+// "HandlerMuleta"
+// InstagramScraper\Exception\InstagramAuthException^ {#1401
+//   #responseBody: ""
+//   #message: "$twoStepVerificator must be an instance of TwoStepVerificationInterface."
+//   #code: 200
+//   #file: "./vendor/raiym/instagram-php-scraper/src/InstagramScraper/Instagram.php"
+//   #line: 2318
+//   trace: {
+//     ./vendor/raiym/instagram-php-scraper/src/InstagramScraper/Instagram.php:2318 { …}
+//     ./vendor/raiym/instagram-php-scraper/src/InstagramScraper/Instagram.php:2194 { …}
+//     /sierra/Dev/Libs/Operador/src/Actions/Instagram/Instagram.php:36 {
+//       Operador\Actions\Instagram\Instagram->__construct($instagram, $cache = false)^
+//       › // dd($this->executor);
+//       › $this->executor->login();
+//       › // $this->executor =  new \InstagramScraper\Instagram(new \GuzzleHttp\Client());
+//     }
+//     /sierra/Dev/Libs/Finder/src/Console/Commands/Spider/InstagramGetAll.php:42 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Container/BoundMethod.php:36 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Container/Util.php:40 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Container/BoundMethod.php:93 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Container/BoundMethod.php:37 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Container/Container.php:653 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Console/Command.php:136 { …}
+//     ./vendor/symfony/console/Command/Command.php:298 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Console/Command.php:121 { …}
+//     ./vendor/symfony/console/Application.php:1024 { …}
+//     ./vendor/symfony/console/Application.php:299 { …}
+//     ./vendor/symfony/console/Application.php:171 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Console/Application.php:94 { …}
+//     ./vendor/laravel/framework/src/Illuminate/Foundation/Console/Kernel.php:129 { …}
+//     ./artisan:35 { …}
+//   }
+// }
 
 class Instagram extends Connector
 {
     protected $targets = [];
+    protected $service;
     protected $executor = false;
     protected $account = false;
+    protected $accountsService = [];
     protected $userName = false;
     public function __construct($instagram, $cache = false)
     {
+        $this->service = app(SocialService::class);
         $this->account = $instagram;
         if (!$this->account) {
             throw new \Exception('Conta não cadastrada para conectar ao instagram');
@@ -48,7 +89,7 @@ class Instagram extends Connector
 
     private function getCache($cache)
     {
-        if (class_exist('\Phpfastcache\Helper\Psr16Adapter')) {
+        if (class_exists('\Phpfastcache\Helper\Psr16Adapter')) {
             return new Psr16Adapter('Files');
         }
         if (!$cache) {
@@ -67,5 +108,52 @@ class Instagram extends Connector
         );
 
         return $this;
+    }
+
+    /**
+     *
+//     $user = [
+//         "pk" => "48689192103"
+//         "username" => "souzaivonelopesde"
+//         "full_name" => "Ivone Lopes de souza"
+//         "is_private" => false
+//         "profile_pic_url" => "https://instagram.fstu1-1.fna.fbcdn.net/v/t51.2885-19/224535572_246257183980017_131335
+// 2671861612954_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fstu1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=Ss9-ggrAl3wAX8HaGD
+// A&edm=AL4D0a4BAAAA&ccb=7-5&oh=00_AT-xg_xiu-WAqAGeb5T1yTIfTTV5vCtrAMbiKTeSISqz5g&oe=6304BA95&_nc_sid=712cc3"
+//         "profile_pic_id" => "2625723166155154417_48689192103"
+//         "is_verified" => false
+//         "has_anonymous_profile_picture" => false
+//         "has_highlight_reels" => false
+//         "account_badges" => []
+//       ] ;
+     */
+    public function registerUserByArray($user)
+    {
+        $pk = $user['pk']|null;
+        $account = null;
+        if(!empty($pk)) {
+            $account = $this->service->setAccount(
+                'Instagram',
+                null,
+                null,
+                null,
+                $pk
+            );
+        }
+        $username = $user['username'];
+        if (empty($username)) {
+            return $account;
+        }
+        if (!isset($this->accountsService[$username])) {
+            $this->accountsService[$username] = $this->service->setAccount(
+                'Instagram',
+                null,
+                null,
+                $username,
+                $pk
+            );
+        }
+
+        return $this->accountsService[$username];
     }
 }
